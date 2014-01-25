7d2b8920f802e13338568eba7c3eda42bae0d0cc
Alexander Chu <alex.p.chu@gmail.com>
2014-01-25 02:28:16 -0800

    more server stuff

--- gameServerAssets/Vector.js
+++ gameServerAssets/Vector.js
@@ -12,6 +12,14 @@ exports.Vector=function (x,y,z){
     this.norm = function(){
         return Math.sqrt(this.x*this.x+this.y*this.y+this.z*this.z)
     }
+    this.projection=function(v){
+        var len=v.norm();
+        if (len==0){
+            return 0;
+        }
+        return this.dotProd(v.scale(1/len));
+    }
+
 }
 exports.add=function(u,v){
     return new Vector(u.x+v.x,u.y+v.y,u.z+v.z);
