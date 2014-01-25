b3aae956df6ba4c595c49876cbe13376083eb364
Alexander Chu <alex.p.chu@gmail.com>
2014-01-25 05:15:19 -0800

    WILL GITHUB FINALLY LETME PUSH?

--- gameServerAssets/Vector.js
+++ gameServerAssets/Vector.js
@@ -4,32 +4,43 @@ Vector=function (x,y,z){
     this.z=z;
     this.getAngle = function(){
         return {
-            'xy':Math.atan(this.z,Math.sqrt(this.x*this.x+this.y*this.y)),
-            'yz':Math.atan(this.x,Math.sqrt(this.y*this.y+this.x*this.x)),
-            'xz':Math.atan(this.y,Math.sqrt(this.x*this.x+this.z*this.z)),
+            'xy':Math.atan2(this.z,Math.sqrt(this.x*this.x+this.y*this.y)),
+            'yz':Math.atan2(this.x,Math.sqrt(this.y*this.y+this.z*this.z)),
+            'xz':Math.atan2(this.y,Math.sqrt(this.x*this.x+this.z*this.z)),
         };
     }
     this.norm = function(){
-        return Math.sqrt(this.x*this.x+this.y*this.y+this.z*this.z)
+        return Math.sqrt(this.x*this.x+this.y*this.y+this.z*this.z);
     }
 }
 add=function(u,v){
-    return new Vector(u.x+v.x,u.y+v.y,u.z+v.z);
+    return new Vector.Vector(u.x+v.x,u.y+v.y,u.z+v.z);
 }
 sub=function(u,v){
-    return new Vector(u.x-v.x,u.y-v.y,u.z-v.z);
+    return new Vector.Vector(u.x-v.x,u.y-v.y,u.z-v.z);
 }
 dot=function(u,v){
     return u.x*v.x+u.y*v.y+u.z*v.z;
 }
 scale=function(v,c){
-    return new Vector(v.x*c,v.y*c);
+    return new Vector.Vector(v.x*c,v.y*c);
 }
 unitVector=function(v){
-    return scale(v,1.0/v.norm());
+    return Vector.scale(v,1.0/v.norm());
+}
+proj = function(u,v){
+    var vUnit = Vector.unitVector(v);
+    Vector.scale(vUnit,Vector.dot(vUnit,u))
+}
+coords={
+    x:new Vector(1,0,0),
+    y:new Vector(0,1,0),
+    z:new Vector(0,0,1),
 }
 
 exports.Vector=Vector;
 exports.add=add;
 exports.sub=sub;
 exports.dot=dot;
+exports.proj=proj;
+exports.coords=coords;
--- index.js
+++ index.js
@@ -2,19 +2,52 @@ var express = require('express')
 var http = require('http')
 var socketio = require('socket.io')
 var constants = require('./gameServerAssets/constants.js').constants;
+var GameState = require('./gameServerAssets/GameState.js').GameState;
 
 var app = express();
 var server = http.createServer(app);
 var io = socketio.listen(server,{log:false});
-server.listen(constants.port);
+server.listen(constants.port,function(){
+    console.log("Server started on port " + constants.port +"\nPress ctrl-c to exit.")
+});
     
+/////////////////////
+//HTTP Server stuff//
+/////////////////////
+
 app.get('/',function(req,res){
     res.sendfile(__dirname + "/" + constants.assetsdir + "/html/index.html");
 });
-
 app.get('/*',function(req,res){
     res.sendfile(__dirname + "/"+ constants.assetsdir +"/" + req.params[0]);
 });
 
+//////////////////
+//GameState Code//
+//////////////////
+
+//Set up individual connections
+var state = new GameState();
 io.sockets.on('connection',function(socket){
+    var sessionKey = makeSessionKey();
+    socket.emit('welcome',{sessionKey:sessionKey});
+    socket.on('command',function(data){
+        state.setControls(data.sessionKey,data.controls);
+    });
+    state.addPlayer(sessionKey);
 });
+
+//Start game heartbeat
+setInterval(function(){
+    io.sockets.emit('heartbeat',state.digest());
+    state.update();
+},constants.gameRefresh);
+
+////////////////////
+//Helper Functions//
+////////////////////
+
+var nextKey=0
+function makeSessionKey(){
+    return nextKey++;
+}
