84af1c68708a722fe5cb4c10bc11574a491accf0
Alexander Chu <alex.p.chu@gmail.com>
2014-01-25 02:46:41 -0800

    added vector coe

--- gameServerAssets/Vector.js
+++ gameServerAssets/Vector.js
@@ -1,4 +1,4 @@
-exports.Vector=function (x,y,z){
+Vector=function (x,y,z){
     this.x=x;
     this.y=y;
     this.z=z;
@@ -12,21 +12,24 @@ exports.Vector=function (x,y,z){
     this.norm = function(){
         return Math.sqrt(this.x*this.x+this.y*this.y+this.z*this.z)
     }
-    this.projection=function(v){
-        var len=v.norm();
-        if (len==0){
-            return 0;
-        }
-        return this.dotProd(v.scale(1/len));
-    }
-
 }
-exports.add=function(u,v){
+add=function(u,v){
     return new Vector(u.x+v.x,u.y+v.y,u.z+v.z);
 }
-exports.sub=function(u,v){
+sub=function(u,v){
     return new Vector(u.x-v.x,u.y-v.y,u.z-v.z);
 }
-exports.dot=function(u,v){
+dot=function(u,v){
     return u.x*v.x+u.y*v.y+u.z*v.z;
 }
+scale=function(v,c){
+    return new Vector(v.x*c,v.y*c);
+}
+unitVector=function(v){
+    return scale(v,1.0/v.norm());
+}
+
+exports.Vector=Vector;
+exports.add=add;
+exports.sub=sub;
+exports.dot=dot;
