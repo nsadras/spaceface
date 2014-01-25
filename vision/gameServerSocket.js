a0c8ee243a14db8fd3256f16ace35977eaba8961
Rohan <ronuchit@yahoo.com>
2014-01-25 15:43:02 -0800

    adding tim's code

---
+++ vision/gameServerSocket.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var http = require('http');
+var socketio = require('socket.io');
+
+var app = express();
+var server = http.createServer(app);
+var io = socketio.listen(server,{log:false});
+
+server.listen(7777,function(){console.log("asdf");});
+
+app.get("/",function(req,res){
+    res.sendfile(__dirname+"/webClientSocket.html");
+})
+io.sockets.on('connection', function (socket){
+	socket.on("IamHere", function (data) {
+		console.log("IamHere!");
+		visionData = data["visionData"]);
+	});
+
+	setInterval(function(){
+		socket.emit("heartbeat", {"currentState": currentState(visionData)}); 
+	},50);
+});
+function currentstate(visionData)
+{
+	
+}
