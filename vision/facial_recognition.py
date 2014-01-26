23460317c93409112dfade1d5cb017cc7688948d
Rohan <ronuchit@yahoo.com>
2014-01-25 17:25:22 -0800

    finished integrating vision server code with game client

--- vision/facial_recognition.py
+++ vision/facial_recognition.py
@@ -3,6 +3,8 @@ import sys
 import numpy as np
 import cv2
 
+NUM_PRINTS = 150 # hack, don't ask
+
 class FacialRecognitionException(Exception):
   pass
 
@@ -19,7 +21,7 @@ class FacialRecognition:
     self.center_x, self.min_x, self.max_x, self.center_y, self.min_y, self.max_y, self.init_dist = self.calibrate()
 
   def calibrate(self):
-    print("Calibration...")
+    sys.stderr.write("Calibration...\n")
     self.cap = cv2.VideoCapture(0)
     try:
       center_x, center_y, init_dist = self.calibrate_center()
@@ -36,17 +38,17 @@ class FacialRecognition:
       sys.exit(-1)
     # self.cap.release()
     # cv2.destroyAllWindows()
-    print("Calibration complete.")
+    sys.stderr.write("Calibration complete.\n")
     return center_x, min_x, max_x, center_y, min_y, max_y, init_dist
 
   def calibrate_center(self):
-    print("Look at camera directly (centered). Press 'c' to continue when face and nose are recognized.")
+    sys.stderr.write("Look at camera directly (centered). Press 'c' to continue when face and nose are recognized.\n")
     center_x, center_y = 0, 0
     while (self.cap.isOpened()):
       ret, frame = self.cap.read()
       frame = cv2.resize(frame, dsize=(0, 0), fx=0.3, fy=0.3)
       if not ret:
-        print("Camera error, quitting.")
+        sys.stderr.write("Camera error, quitting.\n")
         raise FacialRecognitionException()
       gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
       faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
@@ -71,13 +73,13 @@ class FacialRecognition:
         return center_x, center_y, init_dist
 
   def calibrate_edge(self, motion_str):
-    print("Move head slowly %s."%motion_str)
+    sys.stderr.write("Move head slowly %s.\n"%motion_str)
     edge_x, edge_y = 0, 0
     while (self.cap.isOpened()):
       ret, frame = self.cap.read()
       frame = cv2.resize(frame, dsize=(0, 0), fx=0.3, fy=0.3)
       if not ret:
-        print("Camera error, quitting.")
+        sys.stderr.write("Camera error, quitting.\n")
         raise FacialRecognitionException()
       gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
       faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
@@ -107,7 +109,7 @@ class FacialRecognition:
       ret, frame = self.cap.read()
       frame = cv2.resize(frame, dsize=(0, 0), fx=0.3, fy=0.3)
       if not ret:
-        print("Connection with camera lost, quitting.")
+        sys.stderr.write("Connection with camera lost, quitting.\n")
         sys.exit(-1)
       gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
       faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
@@ -141,7 +143,7 @@ class FacialRecognition:
           self.prev_fire = False
 
       if cv2.waitKey(1) & 0xFF == ord("q"):
-        print("Quitting.")
+        sys.stderr.write("Quitting.\n")
         sys.exit(0)
       self.print_features()
 
@@ -149,7 +151,9 @@ class FacialRecognition:
     cv2.destroyAllWindows()
 
   def print_features(self):
-    print "%r   %f   %f   %f"%(self.fire, self.displacement_x, self.displacement_y, self.dist)
+    for _ in range(NUM_PRINTS):
+      print("%r   %f   %f   %f"%(self.fire, self.displacement_x, self.displacement_y, self.dist))
+    sys.stderr.write("%r   %f   %f   %f\n"%(self.fire, self.displacement_x, self.displacement_y, self.dist))
 
 if __name__ == "__main__":
   f = FacialRecognition()
--- vision/gameServerSocket.js
+++ vision/gameServerSocket.js
@@ -5,23 +5,23 @@ var socketio = require('socket.io');
 var app = express();
 var server = http.createServer(app);
 var io = socketio.listen(server,{log:false});
+var visionData;
 
 server.listen(7777,function(){console.log("asdf");});
-
 app.get("/",function(req,res){
     res.sendfile(__dirname+"/webClientSocket.html");
 })
 io.sockets.on('connection', function (socket){
 	socket.on("IamHere", function (data) {
 		console.log("IamHere!");
-		visionData = data["visionData"]);
+		visionData = data["visionData"];
 	});
 
 	setInterval(function(){
 		socket.emit("heartbeat", {"currentState": currentState(visionData)}); 
 	},50);
 });
-function currentstate(visionData)
+function currentState(visionData)
 {
 	
 }
